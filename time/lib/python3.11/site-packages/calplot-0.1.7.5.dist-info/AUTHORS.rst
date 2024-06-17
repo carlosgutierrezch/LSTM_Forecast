fe87ace88da56700c4252d6cc98487b1fcf78c88
Carlos <postcarloscook@gmail.com>
2024-06-17 14:29:18 +0200

    calplot

---
+++ time/lib/python3.11/site-packages/calplot-0.1.7.5.dist-info/AUTHORS.rst
@@ -0,0 +1,6 @@
+Calplot is a fork of Calmap maintained by Tom Kwok.
+
+Calmap was written by Martijn Vermaat.
+
+- Tom Kwok tom [at] tomkwok.com
+- Martijn Vermaat martijn [at] vermaat.name
---
+++ time/lib/python3.11/site-packages/calplot/__init__.py
@@ -0,0 +1,9 @@
+__version_info__ = ('0', '1', '7', '5')
+__date__ = '31 Oct 2022'
+
+__version__ = '.'.join(__version_info__)
+__author__ = 'Tom Kwok'
+__contact__ = 'tom@tomkwok.com'
+__homepage__ = 'https://github.com/tomkwok/calplot'
+
+from .calplot import yearplot, calplot
