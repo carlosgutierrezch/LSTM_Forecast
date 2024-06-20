74129fa7d2f4691d0b1eb60d5e475f3ed32addee
Carlos <postcarloscook@gmail.com>
2024-06-20 13:42:03 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('readme.md','r',encoding='utf-8') as f:
+    long_description= f.read()
+    
+__version__='0.0.0'
+
+REPO_NAME= 'LSTM-Forecast'
+AUTHOR_USER_NAME= 'AETNA-68'
+SRC_REPO= 'Time Series Forecast'
+AUTHOR_EMAIL='aetna68corp@gmail.com'
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description='Ml app',
+    long_description=long_description,
+    long_description_content='text/markdown',
+    url=f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
+    project_urls={
+        'Bug tracker':f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues'
+    },
+    package_dir={'':'src'},
+    packages = setuptools.find_packages(where='src')
+)   
