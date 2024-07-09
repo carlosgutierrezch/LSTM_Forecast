5a65c9af633b3be826eabb97348bd84ff6bf8516
Carlos <postcarloscook@gmail.com>
2024-07-09 14:59:49 +0200

    gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+from TimeSeriesForecast import logger
+from TimeSeriesForecast.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME= 'Data ingestion stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<')
+    obj= DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>>>> stage {STAGE_NAME} completed! <<<<<<')
+except Exception as e:
+    logger.exception(e)
+    raise e
