09d1de2d7bac36b0388d1a29de1a2227950e75ad
Carlos <postcarloscook@gmail.com>
2024-08-05 13:59:28 +0200

    retraining of model

--- notebooks/src/functions_torch.py
+++ notebooks/src/functions_torch.py
@@ -3,6 +3,10 @@ Series of classes for data transformation in LSTM-TORCH module
 """
 import pandas as pd
 import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
 
 class  TransformLag:
     """
@@ -59,3 +63,43 @@ class  TransformLag:
         df.ffill(inplace=True)
         # data_array= df.to_numpy()
         return df
+
+class TimeSeries(Dataset):
+    def __init__(self,X,y):
+        self.X= X
+        self.y= y
+    
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self,i):
+        return self.X[i],self.y[i]
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    
+class LSTM(nn.Module):
+    def __init__(self,input_size,hidden_size,num_stacked_layers):
+        super().__init__()
+        self.hidden_size= hidden_size
+        self.num_stacked_layers= num_stacked_layers
+        
+        self.lstm= nn.LSTM(input_size,hidden_size,num_stacked_layers,
+                           batch_first=True)
+        self.fc= nn.Linear(hidden_size,1)
+        
+    def forward(self,x):
+        batch_size= x.size(0)
+        h0= torch.zeros(self.num_stacked_layers,batch_size,self.hidden_size).to(device)
+        c0= torch.zeros(self.num_stacked_layers,batch_size,self.hidden_size).to(device)
+        
+        out,_ = self.lstm(x,(h0,c0))
+        out= self.fc(out[:,-1,:])
+        return out
+    
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
